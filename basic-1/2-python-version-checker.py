6021a6cf62e913eb6005fc478a634fb083a70ce7
clearcasting <149123708+clearcasting@users.noreply.github.com>
2026-06-27 21:56:21 -0500

    basic task 2

---
+++ basic-1/2-python-version-checker.py
@@ -0,0 +1,10 @@
+"""
+Write a Python program to find out what version of Python you are using.
+"""
+
+import sys
+import platform
+
+print(sys.version)
+print(sys.version_info)
+print(platform.python_version())
