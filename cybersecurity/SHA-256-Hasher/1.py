335e80131a4fd5a257b0ddd92f9503e144e8b1fe
clearcasting <149123708+clearcasting@users.noreply.github.com>
2026-06-30 20:27:00 -0500

    adjusted functionality

--- cybersecurity/SHA-256-Hasher/1.py
+++ cybersecurity/SHA-256-Hasher/1.py
@@ -5,11 +5,14 @@ Write a Python program that defines a function and takes a password string as in
 import hashlib
 
 
-def sha256_hasher(password: bytes) -> str:
+def sha256_hasher(password: str) -> str:
     """
     Returns the hexadecimal string of the sha256 encoded password.
     """
-    return hashlib.sha256(password).digest().hex()
+    encoded_password = password.encode("utf-8")
+    return hashlib.sha256(encoded_password).hexdigest()
 
 
-print(sha256_hasher(b"password"))
+password = input("Enter your password: ")
+hashed_password = sha256_hasher(password)
+print(f"Your SHA-256 hashed password is: {hashed_password}")
