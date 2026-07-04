36a16533d0243d123bf7903f21a88e8a82c7b17c
clearcasting <149123708+clearcasting@users.noreply.github.com>
2026-07-04 18:25:57 -0500

    create a set bonus 1

---
+++ data-types/sets/create-a-set/2.py
@@ -0,0 +1,8 @@
+"""
+Write a Python program to create a set from a list of numbers with duplicates and print the resulting set.
+"""
+
+numbers = [1, 1, 2, 3, 4, 4, 5, 5]
+
+new_set = set(numbers)
+print(new_set)
