867a754e2fc3c876fd8cc74bd7ad538684a9c5c5
clearcasting <149123708+clearcasting@users.noreply.github.com>
2026-07-04 11:05:33 -0500

    lambda exercise 1 bonus 1

---
+++ functional-programming/lambda/lambda-add-multiply/2.py
@@ -0,0 +1,6 @@
+"""
+Write a Python program to create a lambda function that subtracts 15 from a given number and then cubes the result.
+"""
+
+sub_15_cube = lambda a: (a - 15) ** 3
+print(sub_15_cube(17))
