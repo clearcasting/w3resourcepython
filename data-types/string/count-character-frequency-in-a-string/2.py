7bb02458135b4de2e8b8df09901372f09c4cc5bd
clearcasting <149123708+clearcasting@users.noreply.github.com>
2026-07-04 00:26:13 -0500

    character frequency with collecitons.Counter

---
+++ data-types/string/count-character-frequency-in-a-string/2.py
@@ -0,0 +1,8 @@
+"""
+Write a Python program to implement character frequency counting by using collections.Counter.
+"""
+
+import collections
+
+str1 = "google.com"
+print(collections.Counter(str1))
