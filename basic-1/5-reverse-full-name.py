26ca7db3d5da48c95f9f90cb4f904b91cf7d7819
clearcasting <149123708+clearcasting@users.noreply.github.com>
2026-07-11 22:23:01 -0500

    reverse full name

---
+++ basic-1/5-reverse-full-name.py
@@ -0,0 +1,9 @@
+"""
+Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them.
+"""
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+reversed_name = f"{last_name} {first_name}"
+
+print(reversed_name)
