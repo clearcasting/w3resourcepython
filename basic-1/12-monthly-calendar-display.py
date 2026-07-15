d19e3779ece3857b59edb1bd279867054482fea0
clearcasting <149123708+clearcasting@users.noreply.github.com>
2026-07-14 22:22:25 -0500

    monthly calendar display

---
+++ basic-1/12-monthly-calendar-display.py
@@ -0,0 +1,11 @@
+"""
+Write a Python program that prints the calendar for a given month and year.
+Note : Use 'calendar' module.
+"""
+
+import calendar
+
+year = int(input("Enter the year: "))
+month = int(input("Enter the month: "))
+
+print(calendar.month(year, month))
