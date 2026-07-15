82b186f34580103d9e98daf415299e5a48d77e1d
clearcasting <149123708+clearcasting@users.noreply.github.com>
2026-07-15 17:16:44 -0500

    days between dates

---
+++ basic-1/14-days-between-dates.py
@@ -0,0 +1,13 @@
+"""
+Write a Python program to calculate the number of days between two dates.
+Sample dates : (2014, 7, 2), (2014, 7, 11)
+Expected output : 9 days
+"""
+
+import datetime
+
+date1 = datetime.date(2014, 7, 2)
+date2 = datetime.date(2014, 7, 11)
+days = abs(date1 - date2).days
+
+print(f"{days} days")
